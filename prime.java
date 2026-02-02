9b59535a50d258e87622085682f39f8eae85e35a
245123751303-gif <245123751303@mvsrec.edu.in>
2026-02-02 10:43:34 +0000

    DAY1

--- prime.java
+++ prime.java
@@ -1,25 +1,8 @@
-import java.util.Scanner;
-
-public class PrimeNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        boolean isPrime = true;
-
-        if (n <= 1) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i <= n / 2; i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-
-        if (isPrime)
-            System.out.println("Prime");
-        else
-            System.out.println("Not Prime");
+import java.util.*;
+class Prime{
+    public static void main(String[] args){
+        int n=new Scanner(System.in).nextInt(),i;
+        for(i=2;i<=n/2;i++) if(n%i==0) break;
+        System.out.println(n>1 && i>n/2 ? "Prime" : "Not Prime");
     }
 }
