b1a8068bb2b25b6abe2a600a5995cc8525b31af1
245123751303-gif <245123751303@mvsrec.edu.in>
2026-02-11 12:19:17 +0530

    Delete calc.c

--- calc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    char o; float a,b;
-    scanf("%f%c%f",&a,&o,&b);
-    if(o=='+') printf("%f",a+b);
-    if(o=='-') printf("%f",a-b);
-    if(o=='*') printf("%f",a*b);
-    if(o=='/') printf("%f",a/b);
-}
