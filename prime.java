47c044fa37babf869548c9309168d37196190d3f
245123751303-gif <245123751303@mvsrec.edu.in>
2026-03-04 13:30:19 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,8 +1,6 @@
 import java.util.*;
 class Prime{
     public static void main(String[] args){
-        int n=new Scanner(System.in).nextInt(),i;
-        for(i=2;i<=n/2;i++) if(n%i==0) break;
-        System.out.println(n>1 && i>n/2 ? "Prime" : "Not Prime");
+        System.out.println("Not Prime");
     }
 }
